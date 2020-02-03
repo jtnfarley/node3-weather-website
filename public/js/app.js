70935afaee6794a06d831a89a7e63ba4d45263b7
John <jtnfarley@gmail.com>
2020-02-02 20:58:52 -0500

    Ready for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,7 @@ const message2 = document.querySelector('#message2');
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const loc = address.value;
-    fetch('http://localhost:3000/weather?address='+loc).then((response) => {
+    fetch('/weather?address='+loc).then((response) => {
         response.json().then((data) => {
             message1.textContent = data.forecast;
             console.log(data);
--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast = require('./utils/forecast');
 
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 const pub_dir = path.join(__dirname, '../public');
 const views_dir = path.join(__dirname, '../templates/views');
@@ -73,6 +74,6 @@ app.get('*', (req, res) => {
     });
 })
 
-app.listen(3000, () => {
-    console.log('Server started, port 3000');
+app.listen(port, () => {
+    console.log('Server started, port '+port);
 });
