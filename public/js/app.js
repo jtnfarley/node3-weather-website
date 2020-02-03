431dfbe3c852773d9be45812726e7a61c0b3be2e
John <jtnfarley@gmail.com>
2020-02-02 20:11:55 -0500

    Second commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client-side loaded');
-
 const weatherForm = document.querySelector('form');
 const address = document.querySelector('input');
 const message1 = document.querySelector('#message1');
